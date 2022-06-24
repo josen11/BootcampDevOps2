f12bf1540322c2bbdec414a4e40b8e6a6b82398a
José Enrique Aguirre Chavez <jotae_11@hotmail.com>
2022-06-24 18:21:28 -0500

    Add code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Bootcamp DevOps -2da Edición - Ejercicio 1");
+Console.WriteLine("=============================================");
+Console.WriteLine("=============================================");
+Console.WriteLine("Paso 1: Por favor ingresa una palabra para obtener los resultados");
+var cadena = Console.ReadLine();
+Console.WriteLine("=============================================");
+Console.WriteLine("Paso 2: Analizar Resultados ");
+Console.WriteLine($"Mostrar sus dos primeras letras: {cadena.ToString().Substring(0,2)}");
+Console.WriteLine($"Mostrar sus tres primeras letras: {cadena.ToString().Substring(0,3)}");
+Console.WriteLine($"Mostrar sus dos ultimas letras: {cadena.ToString().Substring(cadena.Length-2, 2)}");
+Console.WriteLine($"Mostrar sus dos primeras letras: {cadena.ToString().Substring(cadena.Length-1, 1)}");
+Console.WriteLine("=============================================");
+Console.WriteLine("PD. Espero salir seleccionado =D");
+Console.WriteLine("=============================================");
